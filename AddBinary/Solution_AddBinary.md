8df1df98eca029d742d2f6cbcc2f0564232858c0
youreverydayuser <f.kuster13@gmail.com>
2026-03-17 09:12:11 +0100

    Added test cases for BestTimeToBuyAndSellStock

---
+++ AddBinary/Solution_AddBinary.md
@@ -0,0 +1,25 @@
+# Solution for the Sort Colors
+
+TBD
+
+## 1. Specification-based testing
+### 1. Understand the requirement, inputs, and outputs
+TBD
+
+### 2. Explore the program
+
+### 3. Judiciously explore the possible inputs and outputs, and identify the partitions.
+TBD
+
+### 4. Identify the boundaries
+TBD
+
+### 5. Devise test cases based on the partitions and boundaries
+TBD
+
+### 6. Automate the test cases
+### 7. Augment the test suite with creativity and experience
+
+## 2. Structural testing
+
+## 3. Mutation testing
--- BestTimeToBuyAndSellStock/src/main/java/zest/BestTimeToBuyAndSellStock.java
+++ BestTimeToBuyAndSellStock/src/main/java/zest/BestTimeToBuyAndSellStock.java
@@ -2,6 +2,9 @@ package zest;
 
 public class BestTimeToBuyAndSellStock {
 
+    public static final int UPPER_LIMIT_LENGTH = 100000;
+    public static final int UPPER_LIMIT_VALUES = 10000;
+
     /**
      * Returns the maximum profit that can be achieved by
      * buying on one day and selling on a later day.
@@ -15,15 +18,23 @@ public class BestTimeToBuyAndSellStock {
             throw new IllegalArgumentException("Input array cannot be null or empty");
         }
 
-        int minPrice = 0;
+        if (prices.length > UPPER_LIMIT_LENGTH) {
+            throw new IllegalArgumentException("Input array should have at most 10^5 elements");
+        }
+
+        int minPrice = Integer.MAX_VALUE;
         int maxProfit = 0;
 
-        for (int i = 1; i < prices.length; i++) {
+        for (int price : prices) {
+            if (price < 0 || price > UPPER_LIMIT_VALUES) {
+                throw new IllegalArgumentException(String.format("Value %d is not valid. " +
+                        "The values cannot be negative and should be smaller then 10^4", price));
+            }
 
-            if (prices[i] < minPrice) {
-                minPrice = prices[i];
+            if (price < minPrice) {
+                minPrice = price;
             } else {
-                int profit = prices[i] - minPrice;
+                int profit = price - minPrice;
                 if (profit > maxProfit) {
                     maxProfit = profit;
                 }
